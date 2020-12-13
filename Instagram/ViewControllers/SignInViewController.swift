6a2c5a1f1573f9e4f431a80218d0fddb20f1cfe0
https://github.com/rmeytar/Instagram.git <https://github.com/rmeytar/Instagram.git>
2020-12-13 14:28:06 +0200

    Authentication with Firebase

--- Instagram/ViewControllers/SignInViewController.swift
+++ Instagram/ViewControllers/SignInViewController.swift
@@ -6,11 +6,13 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SignInViewController: UIViewController {
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,7 +38,36 @@ class SignInViewController: UIViewController {
        bottomLayerPassword.backgroundColor = UIColor(red: 55/255, green: 50/255, blue: 25/255, alpha: 1).cgColor
        passwordTextField.layer.addSublayer(bottomLayerPassword)
         
+        handleTextField()
+        
+    }
+    
+    func handleTextField() {
+        
+        emailTextField.addTarget(self, action: #selector(SignUpViewController.textFieldDidChange), for: UIControl.Event.editingChanged)
+        passwordTextField.addTarget(self, action: #selector(SignUpViewController.textFieldDidChange), for: UIControl.Event.editingChanged)
+    }
+    
+    //if one of this is empty - we disable the sign up button.
+    @objc func textFieldDidChange() {
+        guard let email = emailTextField.text, !email.isEmpty,
+              let password = passwordTextField.text, !password.isEmpty else {
+            signInButton.setTitleColor(.lightText, for: UIControl.State.normal)
+            signInButton.isEnabled = false
+            return
+        }
+        signInButton.setTitleColor(.white, for: UIControl.State.normal)
+        signInButton.isEnabled = true
         
     }
 
+    @IBAction func signInButton_TouchUpInside(_ sender: Any) {
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!, completion: { authResult, error in
+            if error != nil {
+                print(error!.localizedDescription)
+                return
+            }
+            self.performSegue(withIdentifier: "signInToTabbarVC", sender: nil)
+        })
+    }
 }
