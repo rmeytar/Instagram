b3eabc379e126d5fd96c8cb4065b8453d89fd548
https://github.com/rmeytar/Instagram.git <https://github.com/rmeytar/Instagram.git>
2020-12-25 22:06:33 +0200

    Like posts

--- Instagram/Views/HomeTableViewCell.swift
+++ Instagram/Views/HomeTableViewCell.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import FirebaseDatabase
+import FirebaseAuth
 
 class HomeTableViewCell: UITableViewCell {
     
@@ -20,6 +21,7 @@ class HomeTableViewCell: UITableViewCell {
     @IBOutlet weak var captionLabel: UILabel!
     
     var homeVC: HomeViewController?
+    var postRef: DatabaseReference!
     
     var post: Post? {
         didSet {
@@ -39,6 +41,33 @@ class HomeTableViewCell: UITableViewCell {
             let photoUrl = URL(string: photoUrlString)
             postImageView.sd_setImage(with: photoUrl)
         }
+        
+        Api.Post.REF_POSTS.child(post!.id!).observeSingleEvent(of: .value, with: {
+            snapshot in
+            if let dict = snapshot.value as? [String: Any] {
+                let post = Post.transformPostPhoto(dict: dict, key: snapshot.key)
+                self.updateLike(post: post)
+            }
+        })
+        Api.Post.REF_POSTS.child(post!.id!).observe(.childChanged, with: {
+            snapshot in
+            if let value = snapshot.value as? Int {
+                self.likeCountButton.setTitle("\(value) likes", for: UIControl.State.normal)
+            }
+        })
+    }
+    
+    func updateLike(post: Post) {
+        let imageName = post.likes == nil || !post.isLiked! ? "like" : "likeSelected"
+        likeImageView.image = UIImage(named: imageName)
+        guard let count = post.likeCount else {
+            return
+        }
+        if count != 0 {
+            likeCountButton.setTitle("\(count) likes", for: UIControl.State.normal)
+        } else {
+            likeCountButton.setTitle("Be the first to like this", for: UIControl.State.normal)
+        }
     }
     
     func setupUserInfo() {
@@ -56,6 +85,52 @@ class HomeTableViewCell: UITableViewCell {
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.commentImageView_TouchUpInside))
         commentImageView.addGestureRecognizer(tapGesture)
         commentImageView.isUserInteractionEnabled = true
+        
+        let tapGestureForLikeImageView = UITapGestureRecognizer(target: self, action: #selector(self.likeImageView_TouchUpInside))
+        likeImageView.addGestureRecognizer(tapGestureForLikeImageView)
+        likeImageView.isUserInteractionEnabled = true
+        
+    }
+    
+    //performs the like action
+    @objc func likeImageView_TouchUpInside() {
+        postRef = Api.Post.REF_POSTS.child(post!.id!)
+        incrementLikes(forRef: postRef)
+    }
+    
+    func incrementLikes(forRef ref: DatabaseReference) {
+
+        ref.runTransactionBlock({ (currentData: MutableData) -> TransactionResult in
+          if var post = currentData.value as? [String : AnyObject], let uid = Auth.auth().currentUser?.uid {
+            var likes: Dictionary<String, Bool>
+            likes = post["likes"] as? [String : Bool] ?? [:]
+            var likeCount = post["likeCount"] as? Int ?? 0
+            if let _ = likes[uid] {
+              // Unlike the post and remove self from likes
+                likeCount -= 1
+                likes.removeValue(forKey: uid)
+            } else {
+              // Like the post and add self to likes
+                likeCount += 1
+                likes[uid] = true
+            }
+            post["likeCount"] = likeCount as AnyObject?
+            post["likes"] = likes as AnyObject?
+            // Set value and report transaction success
+            currentData.value = post
+            return TransactionResult.success(withValue: currentData)
+          }
+          return TransactionResult.success(withValue: currentData)
+        }) { (error, committed, snapshot) in
+          if let error = error {
+            print(error.localizedDescription)
+          }
+            if let dict = snapshot?.value as? [String: Any] {
+                let post = Post.transformPostPhoto(dict: dict, key: snapshot!.key)
+                self.updateLike(post: post)
+            }
+
+        }
     }
     
     //display the comments
